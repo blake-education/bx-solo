d294ac512aa3285ca54cb6a10e0a3f2385e00735
Richard Lane <32445861+rjl79@users.noreply.github.com>
2019-07-29 11:43:58 +1000

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,15 +2,17 @@
 
 set -euo pipefail
 
-# install Xcode Command Line Tools
-# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" --verbose;
+if ! [[ xcode-select -p 1>/dev/null;echo $? ]]; then
+  # install Xcode Command Line Tools
+  # https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" --verbose;
+fi
 
 cat <<EODOC
 laptop setup for the greater good and prohax: phase 1
