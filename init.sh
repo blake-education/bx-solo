d75c2b46ca38b8d86f925dc1a95a1f0ad34bc7f4
Tom Ferguson <111714273+3pl-tferg-devops@users.noreply.github.com>
2025-02-18 12:54:52 +1100

    install asdf first and then install gh via asdf
    - This is because later in the bootstrap process we uninstall the brew
      installation of gh and install the asdf version.

--- init.sh
+++ init.sh
@@ -2,16 +2,25 @@
 
 set -euo pipefail
 
-echo "Installing Github CLI Tools."
-brew install gh
+echo "Installing asdf"
+brew install asdf
 
-echo "Setting you up on github."
+echo "export PATH=\"\$HOME/.asdf/shims:\$PATH\"" >> $HOME/.zshrc
+
+source $HOME/.zshrc
+
+echo "Installing Github CLI Tools"
+asdf plugin add github-cli
+asdf install github-cli latest
+asdf set --home github-cli latest
+
+echo "Setting you up on github"
 gh auth login -p ssh -h github.com --insecure-storage -w
 
 if [[ ! -d ~/Blake/bx ]]; then
-  echo "Cloning bx repo."
+  echo "Cloning bx repo"
   git clone git@github.com:blake-education/bx.git ~/Blake/bx
 fi
 
-echo "Starting mac-bootstrap."
+echo "Starting mac-bootstrap"
 exec ~/Blake/bx/bin/bx mac-bootstrap
