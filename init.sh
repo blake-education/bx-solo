3c6ee13bf87dae275db42f15160af428c5d0cee0
Tom Ferguson <111714273+3pl-tferg-devops@users.noreply.github.com>
2025-02-19 09:24:27 +1100

    adds some checks to make sure the dependencies are installed

--- init.sh
+++ init.sh
@@ -2,16 +2,38 @@
 
 set -euo pipefail
 
-echo "Installing Github CLI Tools."
-brew install gh
+xcode_tools_installed() {
+  pkgutil --pkg-info=com.apple.pkg.CLTools_Executables 1>/dev/null 2>&1
+}
 
-echo "Setting you up on github."
-gh auth login -p ssh -h github.com --insecure-storage -w
+brew_installed() {
+  brew --version 1>/dev/null 2>&1
+}
 
-if [[ ! -d ~/Blake/bx ]]; then
-  echo "Cloning bx repo."
-  git clone git@github.com:blake-education/bx.git ~/Blake/bx
+if xcode_tools_installed && brew_installed; then
+
+  echo "Installing Github CLI Tools."
+  brew install gh
+
+  echo "Setting you up on github."
+  gh auth login -p ssh -h github.com --insecure-storage -w
+
+  if [[ ! -d ~/Blake/bx ]]; then
+    echo "Cloning bx repo."
+    git clone git@github.com:blake-education/bx.git ~/Blake/bx
+  fi
+
+  echo "Starting mac-bootstrap."
+  exec ~/Blake/bx/bin/bx mac-bootstrap
+
+else
+
+  if ! xcode_tools_installed; then
+    echo "Xcode tools is not installed. Run xcode-select --install."
+
+  elif ! brew_installed; then
+    echo "Brew is not installed. Kandji Liftoff may not have run yet. Contact #devops in slack"
+
+  fi
+
 fi
-
-echo "Starting mac-bootstrap."
-exec ~/Blake/bx/bin/bx mac-bootstrap
